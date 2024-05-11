44e8aa2fc1c9c3493d9847e3cd383589cceec0d7
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-05-11 22:37:03 +0100

    hw_2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '43151566-329d1ba35c4912218880d8d24';
+const API_KEY = '43788536-1320d3af2b7eba5483923831a';
 
 export const getImages = (searchValue = '') => {
   return fetch(
